2fecde5bcf8144d96468e85a8f68ada5706b6e7d
Elisabete <38789950+Ebernardinof@users.noreply.github.com>
2021-04-27 14:37:42 +0100

    contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,6 +5,7 @@ import FormControl from "@material-ui/core/FormControl";
 import InputLabel from "@material-ui/core/InputLabel";
 import TextField from "@material-ui/core/TextField";
 import useWindowPosition from "../hook/useWindowPosition";
+import { Input } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -42,81 +43,51 @@ export default function Contact() {
     setState({ ...state, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const form = e.target;
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({
-        "form-name": form.getAttribute("name"),
-        ...state,
-      }),
-    })
-      .then(() => console.log("Form successfully submitted"))
-      .catch((error) => alert(error));
-  };
-
   return (
-    <div className={classes.root} id="contact">
+    <div className={classes.root} id="contactForm">
       <h1>Contact</h1>
-      <form
-        name="contact"
-        method="post"
-        action="/thanks/"
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
-        onSubmit={handleSubmit}
-      >
+      <form name="contact" method="post" data-netlify="true" onSubmit="submit">
         {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-        <input type="hidden" name="form-name" value="contact" netlify="true" />
-        <p hidden>
+        <input type="hidden" name="form-name" value="contact" />
+        {/* <p hidden>
           <label>
             Don’t fill this out:{" "}
             <input name="bot-field" onChange={handleChange} />
           </label>
-        </p>
-        <FormControl variant="outlined">
-          <InputLabel htmlFor="component-outlined">Name</InputLabel>
-          <OutlinedInput
-            id="component-outlined"
-            onChange={handleChange}
-            label="Your Name"
-            name="name"
-          />
-        </FormControl>
-        <FormControl variant="outlined">
-          <InputLabel htmlFor="component-outlined">Email</InputLabel>
-          <OutlinedInput
-            id="component-outlined"
-            onChange={handleChange}
-            label="Your Email"
-            name="email"
-          />
-        </FormControl>
-        <TextField
-          id="outlined-multiline-static"
-          label="Message"
-          multiline
-          rows={4}
-          variant="outlined"
-        />
-        {/* <p>
-          <label>
-            Your email:
-            <br />
-            <input type="email" name="email" onChange={handleChange} />
-          </label>
-        </p>
-        <p>
-          <label>
-            Message:
-            <br />
-            <textarea name="message" onChange={handleChange} />
-          </label>
         </p> */}
+        <div>
+          <FormControl variant="outlined">
+            <InputLabel htmlFor="name">Name</InputLabel>
+            <OutlinedInput
+              id="name"
+              onChange={handleChange}
+              label="Your Name"
+              name="name"
+              type="text"
+            />
+          </FormControl>
+          <FormControl variant="outlined">
+            <InputLabel htmlFor="email">Email</InputLabel>
+            <OutlinedInput
+              id="email"
+              onChange={handleChange}
+              label="Your Email"
+              name="email"
+              type="email"
+            />
+          </FormControl>
+          <TextField
+            id="message"
+            label="Message"
+            multiline
+            rows={4}
+            variant="outlined"
+            name="message"
+          />
+        </div>
+
         <p>
-          <button type="submit">Send</button>
+          <button type="submit">Send it!</button>
         </p>
       </form>
     </div>
