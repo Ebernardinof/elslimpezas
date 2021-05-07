c106593775f9151362d8f2dd50d35f5337a2f306
Elisabete <38789950+Ebernardinof@users.noreply.github.com>
2021-05-07 12:20:10 +0100

    seo

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Services from "./components/Services";
 import { useEffect } from "react";
+
+import { Helmet } from "react-helmet";
+
 import ReactGA from "react-ga";
 ReactGA.initialize("271669041");
 
@@ -25,6 +28,7 @@ const useStyles = makeStyles((theme) => ({
 
 function App() {
   const classes = useStyles();
+  console.log(Helmet);
 
   useEffect(() => {
     ReactGA.pageview(window.location.pathname + window.location.search);
@@ -33,6 +37,16 @@ function App() {
   return (
     <div className={classes.root}>
       <CssBaseline />
+      <Helmet>
+        <meta charSet="utf-8" />
+
+        <title>Els Limpezas</title>
+        <link rel="canonical" href="https://elslimpezas.netlify.app" />
+        <meta
+          name="description"
+          content="Serviços de Limpeza,  Prestamos Serviços em: A Els Limpezas está presente na seguintes áreas: Margem Sul do Tejo: Almada, Seixal, Fernão Ferro, Quinta do Conde, Setúbal, Palmela,Azeitão, Aroeira, Costa da Caparica, Sobreda, Charneca da Caparica, Corroios, Sta. Marta, Amora, Fogueteiro, Cruz de Pau, Torre da Marinha, Barreiro, Montijo, Pinhal Novo, Alcochete, Verdizela, Belverde, Marisol, Arrentela."
+        />
+      </Helmet>
       <Header />
       <Services />
       <div className={classes.container}>
